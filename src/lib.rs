33197bece45ead223c8cb2e6626cf051bf5caeea
Kevin Ramdath <krpent@gmail.com>
2022-06-14 22:00:51 -0400

    move code to a library

---
+++ src/lib.rs
@@ -0,0 +1,73 @@
+use minreq::{Error, Request, Response};
+use regex::Regex;
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+pub struct Manga {
+    pub id: String,
+    pub name: String,
+    pub slug: String,
+}
+
+pub fn get_mangas() -> Vec<Manga> {
+    let html: String = get_mangas_page().expect("Unable to get mangas from site.");
+    get_mangas_from_page(&html)
+}
+
+pub fn get_chapters(manga: &Manga) -> Vec<Manga> {
+    let html = get_chapters_page(&manga).expect("Unable to get chapters from site.");
+    get_chapters_from_page(&html)
+}
+
+fn get_page(url: &str) -> Result<String, Error> {
+    let req: Request = minreq::get(url);
+    let res: Response = req.send()?;
+    let body: &str = res.as_str()?;
+    Ok(String::from(body))
+}
+
+fn get_mangas_page() -> Result<String, Error> {
+    get_page("https://onepiecechapters.com/projects")
+}
+
+fn get_chapters_page(manga: &Manga) -> Result<String, Error> {
+    let url: String = format!(
+        "https://onepiecechapters.com/mangas/{}/{}",
+        manga.id, manga.slug
+    );
+    get_page(&url)
+}
+
+fn get_mangas_from_page(page: &str) -> Vec<Manga> {
+    let re: Regex = Regex::new(r#"href="/mangas/(\d*)/(.*?)">([^<]*)<"#).unwrap();
+
+    let mut mangas: Vec<Manga> = Vec::new();
+    for cap in re.captures_iter(page) {
+        if cap[3].trim().is_empty() {
+            continue;
+        }
+        mangas.push(Manga {
+            id: cap[1].to_string(),
+            name: cap[3].to_string(),
+            slug: cap[2].to_string(),
+        });
+    }
+    mangas
+}
+
+fn get_chapters_from_page(page: &str) -> Vec<Manga> {
+    let re: Regex = Regex::new(r#"(?s)href="/chapters/(\d*)/([^"]*)"[^>]*>(.*?)</a>"#).unwrap();
+
+    let mut chapters: Vec<Manga> = Vec::new();
+    for cap in re.captures_iter(page) {
+        if cap[3].trim().is_empty() {
+            continue;
+        }
+        chapters.push(Manga {
+            id: cap[1].to_string(),
+            name: cap[3].to_string(),
+            slug: cap[2].to_string(),
+        });
+    }
+    chapters
+}
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use clap::{Parser, Subcommand};
+use tcbscans::{get_chapters, get_mangas, Manga};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    action: Actions,
+}
+
+#[derive(Subcommand, Debug)]
+enum Actions {
+    /// List all supported mangas
+    Mangas,
+    /// List all chapters for a supported manga
+    Chapters {
+        /// Slug name of the manga
+        #[clap(short, long)]
+        slug: Option<String>,
+        /// Id of the chapter
+        #[clap(short, long)]
+        id: Option<u8>,
+    },
+}
+
+fn main() {
+    let cli = Args::parse();
+    match &cli.action {
+        Actions::Mangas => {
+            let mangas: Vec<Manga> = get_mangas();
+            for manga in mangas {
+                let json = serde_json::to_string(&manga)
+                    .expect(&format!("Unable to serialize manga: {:?}", &manga));
+                println!("{}", &json);
+            }
+        }
+        Actions::Chapters { slug, id } => {
+            let mangas: Vec<Manga> = get_mangas();
+            let mut manga: Option<&Manga> = None;
+            if let Some(slug) = slug {
+                manga = mangas
+                    .iter()
+                    .find(|&m| &m.slug == slug);
+            } 
+            if manga.is_none() {
+                if let Some(id) = id {
+                    let id: String = format!("{}", id);
+                    manga = mangas
+                        .iter()
+                        .find(|&m| &m.id == &id);
+                }
+            } 
+            
+            let manga: &Manga = manga
+                .expect("Unable to find manga using slug or id.");
+            let chapters = get_chapters(manga);
+            for chapter in chapters {
+                let json = serde_json::to_string(&chapter).unwrap();
+                println!("{}", &json)
+            }
+        }
+    }
+}
